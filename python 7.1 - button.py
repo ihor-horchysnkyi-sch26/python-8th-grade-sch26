4c66cb1fb0cb92483834c5d0714df6a5df2c5afb
Ihor Horchynskyi <ihor.horchynskyi@dataart.com>
2025-01-06 09:06:57 +0200

    lesson 7

---
+++ python 7.1 - button.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('300x200+350+200')
+
+label = Label(text = 'Я навчаюсь у 8 класі',
+
+bg = 'yellow', fg = 'green', font = 'Arial 14')
+label.pack()
