f7ee94a02c5eacc5b30f972c5fde29e7200deafa
Ihor Horchynskyi <ihor.horchynskyi@dataart.com>
2025-01-06 09:33:52 +0200

    lesson 7 part 2

---
+++ python 7.2 - button.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+def label_click (event):
+    label['text'] = 'Я навчаюсь у 8 класі'
+    label['font'] = 14
+    label['fg'] = 'red'
+    label['bg'] = 'yellow'
+
+root = Tk()
+root.geometry('300x200')
+
+label = Label(text = 'Це напис')
+label.bind('<1>', label_click)
+label.pack(pady = 20)
